61c3629405ad22ca356afcf0b74e29c6eb85c036
Azugot <108095759+Azugot@users.noreply.github.com>
2022-06-23 17:58:44 -0300

    Update Arduino_1.ino

--- Arduino_1.ino
+++ Arduino_1.ino
@@ -1,52 +1,39 @@
 // C++ code
 //
-/*
-  Button
-  Turns on and off a light emitting diode(LED)
-  connected to digital  pin 13, when pressing a
-  pushbutton attached to pin 2.
-
-   The circuit:
-  * LED attached from pin 13 to ground
-  * pushbutton attached to pin 2 from +5V
-  * 10K resistor attached to pin 2 from ground
-  * Note: on most Arduinos there is already an LED
-  on the board  attached to pin 13.
-
-  created 2005  by DojoDave <http://www.0j0.org>
-
-  modified 30 Aug 2011  by Tom Igoe
-
-  This example code is in the public domain.
-  http://www.arduino.cc/en/Tutorial/Button
-*/
-
-int buttonState = 0;
+int otherArduinoState = 0;
+int thisButtonState = 0;
 
 void setup()
 {
-  pinMode(2, INPUT);
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(7, INPUT); //Other arduino signal input
+  pinMode(4, OUTPUT); //THIS arduino signal Output
+  pinMode(11, OUTPUT); //LED OUTPUT
+  pinMode(9, INPUT); //Button signal Input
+
+  
 }
 
 void loop()
 {
-  // read the state of the pushbutton value
-  buttonState = digitalRead(2);
-  // check if pushbutton is pressed.  if it is, the
-  // buttonState is HIGH
-  if (buttonState == HIGH) {
-    // turn LED on
-    digitalWrite(7, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(11, HIGH);
-  } else {
-    // turn LED off
-    digitalWrite(7, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(11, LOW);
+  otherArduinoState = digitalRead(7); //State of the other arduino signal
+  if (otherArduinoState == HIGH) {    //if signal recieved
+    digitalWrite(11, HIGH);           //Send signal to THIS LED
+  } 
+  else {
+    digitalWrite(11, LOW);            //Deactivate LED
   }
-  delay(10); // Delay a little bit to improve simulation performance
+  
+//=================================================================
+
+  thisButtonState = digitalRead(9); // ButtonState = THIS button signal
+  if (thisButtonState == HIGH) {    // If button is pressed
+    digitalWrite(4, HIGH);          //Send signal to other arduino
+    digitalWrite(11, HIGH);         //Send signal to THIS LED
+
+  }
+  else{
+    digitalWrite(4, LOW);           //Deactivate both signals
+    digitalWrite(11, LOW);          //
+  }
+
 }
